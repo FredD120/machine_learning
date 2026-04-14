8dd10ad4d10819b4cb3dc1f61194520e3b93b38a
FredD120 <fw.daniels01@gmail.com>
2026-04-14 22:49:25 +0100

    import original pst evaluation scores from file and set up dot product to reproduce board evaluation score. speed this process up 3-5x by only visiting non-zero features

--- src/machine_learning.jl
+++ src/machine_learning.jl
@@ -1,15 +1,44 @@
 using HDF5
 using GLMakie
 using Scylla
-using Threads
+using .Threads
+using BenchmarkTools
+
+const SLICE = 1_000_000
+
+"Retrieve piece square tables from file"
+function get_pst(type)
+    h5open("$(dirname(@__DIR__))/PST/$(type).h5", "r") do fid
+        MG::Vector{Float32} = read(fid["MidGame"])
+        EG::Vector{Float32} = read(fid["EndGame"])
+        return (MG, EG)
+    end
+end
+
+function get_evaluation()
+    (kingMG, kingEG) = get_pst("king")
+    (queenMG, queenEG) = get_pst("queen")
+    (rookMG, rookEG) = get_pst("rook")
+    (bishopMG, bishopEG) = get_pst("bishop")
+    (knightMG, knightEG) = get_pst("knight")
+    (pawnMG, pawnEG) = get_pst("pawn")
+
+    eval = Float32[]
+
+    for vec in [kingMG, queenMG, rookMG, bishopMG, knightMG, pawnMG,
+                kingEG, queenEG, rookEG, bishopEG, knightEG, pawnEG]
+        for v in vec
+            push!(eval, v)
+        end
+    end
+    return eval
+end
 
 struct Position
-    features::Vector{Float32}
-    score::Int8
-
-    function Position(f::Vector{Float32}, s::Int8)
-        return New(f, s)
-    end
+    features::Vector{Float32} # (white occupied positions) - (black occupied positions), for mid and endgame
+    score::Int8               # score of position in actual game (win/draw/loss)
+    occupied::Vector{UInt32}  # only visit feautures that are non-zero
+    original_eval::Int16      # for testing evaluation dot product
 end
 
 function Position(fen::S, result::S) where {S <: AbstractString}
@@ -39,19 +68,46 @@ function Position(fen::S, result::S) where {S <: AbstractString}
     elseif occursin("1-0", result)
         score = -1
     end
+    
+    occupied = UInt32[]
+    for (i, f) in enumerate(features)
+        f != 0 ? push!(occupied, i) : nothing
+    end
 
-    return Position(features, score)
+    return Position(features, score, occupied, Scylla.evaluate(board))
 end
 
 function get_features()
     data = readlines(pwd() * "\\data\\zurichess.epd")
 
     positions = Vector{Position}(undef, length(data))
-    Threads.@threads for i in eachindex(data[1:100])
+    #=@threads=# for i in eachindex(data[1:SLICE])
         arr = split(data[i], "c9")
         positions[i] = Position(arr[1], arr[2])
     end
     return positions
 end 
-pos = get_features()
-println(pos[2])
+
+function dot(pos::Position, pst)
+    count = Float32(0)
+    for i in pos.occupied
+        count += pos.features[i] * pst[i]
+    end
+    return count
+end
+
+function evaluate_all(pos, eval)
+    count = 0.0
+    for p in @view pos[1:SLICE]
+        count += dot(p, eval)
+    end
+    return count
+end
+
+function main()
+    @time pos = get_features()
+    eval = get_evaluation()
+    @btime c = evaluate_all($pos, $eval)
+    #evaluate_all(pos, eval)
+end
+main()
