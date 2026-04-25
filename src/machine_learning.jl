d2f546680c34f87117a8c7e43afd43874204de51
Freddie-Durham <wzjr67@durham.ac.uk>
2026-04-25 12:07:59 +0100

    add sigmoid function and plot loss curve

--- src/machine_learning.jl
+++ src/machine_learning.jl
@@ -6,6 +6,7 @@ import .Threads
 import Serialization
 
 const SLICE = 1_428_000 # max size zurichess = 1_428_000
+const K_VALUE = 3.0 / 500 # hyperparameter in sigmoid function
 
 "Retrieve piece square tables from file"
 function get_pst(type)
@@ -37,7 +38,7 @@ end
 
 struct Position
     features::Vector{Float32} # (white occupied positions) - (black occupied positions), for mid and endgame
-    score::Int8               # score of position in actual game (win/draw/loss)
+    score::Float32            # score of position in actual game (win/draw/loss)
     occupied::Vector{UInt32}  # only visit feautures that are non-zero
     original_eval::Int16      # for testing evaluation dot product
 end
@@ -63,11 +64,11 @@ function Position(fen::S, result::S) where {S <: AbstractString}
         end
     end
 
-    score = 0
-    if occursin("0-1", result)
-        score = 1
-    elseif occursin("1-0", result)
-        score = -1
+    score = 0.5
+    if occursin("1-0", result) # white wins
+        score = 1.0
+    elseif occursin("0-1", result) # black wins
+        score = 0.0
     end
     
     occupied = UInt32[]
@@ -98,29 +99,57 @@ function dot(pos::Position, pst)
     for (linear_index, jump_index) in enumerate(pos.occupied)
         count += pos.features[linear_index] * pst[jump_index]
     end
-    @assert floor(Int16, count) == pos.original_eval
+    #@assert floor(Int16, count) == pos.original_eval
     return count
 end
 
-function evaluate_all(pos, eval)
-    count = 0.0
-    for p in @view pos[1:SLICE]
-        count += dot(p, eval)
+sigmoid(x, k) = 1 / (1 + exp(-k * x))
+
+calculate_loss(feature, eval, k) = sigmoid(dot(feature, eval), k) - feature.score
+
+function mean_squared_error(feature_set, eval)
+    error = Float32(0.0)
+    for feature in @view feature_set[1:SLICE]
+        loss = calculate_loss(feature, eval, K_VALUE)
+        error += loss * loss
     end
-    return count
+    return error / SLICE
 end
 
 function create_feature_data(filename)
-    pos = get_features()
-    Serialization.serialize(filename, pos)
+    features = get_features()
+    Serialization.serialize(filename, features)
+end
+
+function plot_loss(feature_set, eval, BUCKET_COUNT = 100)
+    buckets = zeros(BUCKET_COUNT)
+    for feature in feature_set
+        loss = calculate_loss(feature, eval, K_VALUE)
+        ind = floor(Int16, (BUCKET_COUNT - 1) * (loss + 1) / 2) + 1
+        buckets[ind] += 1
+    end
+    xs = [-1.0 + 2 * i / BUCKET_COUNT for i in 1:BUCKET_COUNT]
+    barplot(xs, buckets)
+end
+
+function plot_evaluation(feature_set, BUCKET_COUNT = 100)
+    buckets = zeros(BUCKET_COUNT)
+    for feature in feature_set
+        ind = floor(Int16, (BUCKET_COUNT - 1) * (1 + feature.original_eval / 900) / 2)
+        ind = min(BUCKET_COUNT, max(1, ind + 1))
+        buckets[ind] += 1
+    end
+    barplot(buckets)
 end
 
 function main()
     filename = "$(dirname(@__DIR__))/data/zurichess_serialize"
     #@time create_feature_data(filename)
-    @time pos = Serialization.deserialize(filename)
+    @time feature_set = Serialization.deserialize(filename)
     eval = get_evaluation()
-    @btime c = evaluate_all($pos, $eval)
-    #evaluate_all(pos, eval)
+    #@btime c = evaluate_all($feature_set, $eval)
+    #@btime mean_squared_error($feature_set, $eval)
+    plot_loss(feature_set, eval)
+    #plot_evaluation(feature_set)
 end
 main()
